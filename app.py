e46e471aeb36e88a458e81adf6c5822a67d741ab
Ayush Jha <145047690+aj-codespy@users.noreply.github.com>
2025-01-31 21:31:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title('Law Buddy: Your Personal Lawyer')
 
 st.write('Ask any Law based question and get answer within seconds!')
 
-query = st.text_input('Enter your question: ')
+query = st.text_input('Enter your question:', key="law_query_input")
 
 if st.button('Enter'):
     st.write(retrieve_and_answer(query))
